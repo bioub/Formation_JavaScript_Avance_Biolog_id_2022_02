cb0bc5f429e8fa347fca14b22a714eda10092d53
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2022-02-10 12:22:26 +0100

    Exercice

--- Serveur/02-express.js
+++ Serveur/02-express.js
@@ -5,16 +5,27 @@ const cors = require('cors');
 const app = express()
 const port = 3000
 
+const todos = [{id: 1, title: 'ABC', completed: false}];
+
 app.use(morgan('dev'));
 app.use(cors());
+app.use(express.json()); // parser le json du body de la requete
+// seulement si Content-type: application/json
+// et fourni le resultat dans req.body
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/todos', (req, res) => {
-  res.json([{id: 1, title: 'ABC', completed: false}]);
+  res.json(todos);
 })
+
+// Exercice
+// traiter le POST de /todos
+// recupérer la todo dans req.body
+// la stocker dans le tableau en lui générant un id
+// retourner en JSON la todo avec le nouvel id
 
 // app.post('/', (req, res) => {
 //   res.send('Hello World!')
--- Todos/src/api.js
+++ Todos/src/api.js
@@ -4,6 +4,16 @@
 // }
 
 export async function fetchTodos() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+  const res = await fetch('http://localhost:3000/todos');
   return await res.json();
 }
+
+// Exercice
+// Créer une fonction postTodo qui prend en paramètre une todo
+// au format {title: 'ABC', completed: false}
+// Cette fonction doit appeler via fetch (voir la doc sur MDN)
+// l'url http://localhost:3000/todos en POST
+// et ajouter l'entête de requete Content-type: application/json
+// et le body doit être la todo en JSON
+
+// Appeler cette fonction au submit du formulaire
